9361f951cd84422e8cef8d173d3f17f54f0c2456
Reema Alsubaie <reemar0o08@gmail.com>
2025-09-28 22:47:34 +0300

    Update CustomerServiceImpl.java

--- backend/src/main/java/com/example/demo/serviceImpl/CustomerServiceImpl.java
+++ backend/src/main/java/com/example/demo/serviceImpl/CustomerServiceImpl.java
@@ -5,8 +5,11 @@ import com.example.demo.repo.CustomerRepository;
 import com.example.demo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.context.annotation.Profile;
+
 
 import java.util.List;
+@Profile("!test-no-db")
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
