5b1495bc9e08c9768f4c0b41a3701fa535016d9f
Reema Alsubaie <reemar0o08@gmail.com>
2025-10-02 18:47:05 +0300

    Update app.component.spec.ts

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,11 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-//ADD
 import { FormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CustomerService, Customer } from './customer.service';
 import { of } from 'rxjs';
-//ADD
+
 describe('AppComponent', () => {
   const customerServiceMock = {
     getCustomers: () => of([] as Customer[]),
@@ -26,7 +25,7 @@ describe('AppComponent', () => {
     expect(fixture.componentInstance).toBeTruthy();
   });
 
-  it('should have title 'frontend', () => {
+  it(`should have title 'frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     expect(fixture.componentInstance.title).toEqual('frontend');
   });
